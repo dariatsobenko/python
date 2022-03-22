2a5f2bdb968589fef13433b31111d0a5cc9963fc
dariatsobenko <99836236+dariatsobenko@users.noreply.github.com>
2022-03-23 01:29:24 +0300

    Update laboratornaya.py

--- hw/laboratornaya.py
+++ hw/laboratornaya.py
@@ -36,5 +36,74 @@ while True:
      if df == wr['number']:
       ret = py
   print('№: ', wr['number'], ', тип: ', wr['type'], ', владелец: ', wr['name'], ', полка хранения:', ret)
+  if a == 'ads':
+  er = input('Введите номер полки: ')
+  print('Результат:')
+  if er in directories.keys():
+   dsa = []
+   for fde in directories:
+    dsa.append(fde)
+   esd = ','.join(dsa)
+   print('Такая полка уже существует. Текущий перечень полок: ', esd, '.')
+  else:
+   rt = {er : []}
+   directories.update(rt)
+   rsd = []
+   for tr in directories:
+    rsd.append(tr)
+   rsde = ','.join(rsd)
+   print('Полка добавлена. Текущий перечень полок: ', rsde, '.')
+ if a == 'ds':
+  dcd = input('Введите номер полки: ')
+  print('Результат: ')
+  if dcd not in directories.keys():
+   rdd = []
+   for www in directories:
+    rdd.append(www)
+   ess = ','.join(rdd)
+   print('Такой полки не существует. Текущий перечень полок: ', ess, '.')
+  else:
+   if directories[dcd] == []:
+    directories.pop(dcd)
+    dds = []
+    for sds in directories:
+     dds.append(sds)
+    dsf = ','.join(dds)
+    print('Полка удалена. Текущий перечень полок: ',dsf, '.')
+   else:
+    rtt = []
+    for fff in directories:
+     rtt.append(fff)
+    xdfg = ','.join(rtt)
+    print('На полке есть документы, удалите их перед удалением полки. '
+          'Текущий перечень полок: ',xdfg, '.')
+ if a == 'ad':
+  sfg = input('Введите номер документа: ')
+  typer = input('Введите тип документа: ')
+  namer = input('Введите владельца документа: ')
+  polka = input('Введите полку для хранения: ')
+  print('Результат:')
+  if polka not in directories.keys():
+   print('Такой полки не существует. Добавьте полку командой as.')
+   print('Текущий список документов: ')
+   for dfg in documents:
+    for esr in directories:
+     for sad in directories[esr]:
+      if sad == dfg['number']:
+       hjg = esr
+    print('№: ', dfg['number'], ', тип: ', dfg['type'], ', владелец: ', dfg['name'], ', полка хранения:', hjg)
+  else:
+   newd = {'type': typer,'number': sfg, 'name': namer}
+   documents.append(newd)
+   for tru in directories:
+    if tru == polka:
+     directories[tru].append(sfg)
+   print('Документ добавлен. Текущий список документов:')
+   for rty in documents:
+    for zxc in directories:
+     for tyu in directories[zxc]:
+      if tyu == rty['number']:
+       jkl = zxc
+    print('№: ', rty['number'], ', тип: ', rty['type'], ', владелец: ', rty['name'], ', полка хранения:', jkl)
  if a == 'q':
   break
