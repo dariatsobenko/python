6657e3a3dc82b4a97a8ad2f44efdfc9a9dc942e3
dariatsobenko <99836236+dariatsobenko@users.noreply.github.com>
2022-04-06 21:00:28 +0300

    Update laboratornya2.py

--- hw/laboratornya2.py
+++ hw/laboratornya2.py
@@ -13,7 +13,21 @@ newstudent("C", "R")
 def findstudent(x, y):
     if y == '':
         f = open('C:\students.txt', 'r')
-
+        for item in f:
+            if x in item:
+                print(item)
+        f.close()
+    else:
+        f = open('C:\students.txt', 'r')
+        for ui in f:
+            if ui == x + " " + y:
+                s = 1
+            else:
+                s = 0
+        if s == 1:
+            print('Студент находится в данной группе')
+        else:
+            print('Студент находится не в данной группе')
 
 
 
